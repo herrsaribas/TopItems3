d657c1b69e0406bbf8c7458f792eed94b63ef644
herrsaribas <ahmet.saribas@hafenx.com>
2025-10-20 13:54:25 +0200

    feat: updated TopItems controller and twig templates

--- src/Providers/HelloWorldRouteServiceProvider.php
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -16,6 +16,7 @@ class HelloWorldRouteServiceProvider extends RouteServiceProvider
      */
     public function map(Router $router)
     {
-        $router->get('hello-world','HelloWorld\Controllers\HelloWorldController@getHelloWorldPage');
+        $router->get('hello-world', 'HelloWorld\Controllers\ContentController@getHelloWorldPage');
+        $router->get('topitems', 'TopItems\Controllers\TopItemsController@showTopItems');
     }
 }
